402d7783e1f3302c5e7c5a6e50877fe3d7636b5e
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-05-03 14:56:08 +0530

    package manager added and parcel,react,react-dom added as packages

--- App.js
+++ App.js
@@ -1,1 +1,30 @@
+import react from 'react';
+import reactDom from 'react-dom/client';
 
+let header = react.createElement(
+  'h1',
+  {
+    id: 'header',
+  },
+  'Hello namaste React'
+);
+
+let header2 = react.createElement(
+  'h2',
+  {
+    id: 'header2',
+  },
+  'header2'
+);
+
+let container = react.createElement(
+  'div',
+  {
+    id: 'div',
+  },
+  [header, header2]
+);
+
+let root = reactDom.createRoot(document.getElementById('root'));
+
+root.render(container);
